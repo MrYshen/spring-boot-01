70bd3d2a61ca98d4fe301f83610fab4b76d8ee2f
Mr.Yshen <351171891@qq.com>
2019-11-14 11:12:15 +0800

    spring-boot-01

---
+++ src/main/java/edu/hust/springboot/controller/HelloController.java
@@ -0,0 +1,20 @@
+package edu.hust.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**@Controller
+@ResponseBody
+*/
+@RestController
+public class HelloController {
+
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+
+}
